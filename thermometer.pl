6bd5257e7992cbb1af8c9b54a4c562e4a99ba874
Phil Windley <github@windley.org>
2012-08-23 14:27:33 -0600

    multiple changes to support directives

--- thermometer.pl
+++ thermometer.pl
@@ -122,10 +122,14 @@ sub run_temperature_task(@) {
 
 
     warning "Starting temperature daemon";
+    # initialization
+    radstat_request($ua, "$thermo_url", "initialization", {t_type_post => 1} );
+
 
     while(1) {   
 
       my $res = $ua->request($req);
+
 
       if ($res->is_success) {
 
@@ -134,6 +138,14 @@ sub run_temperature_task(@) {
 	warning "Temperature is " . $scalar->{ 'temp' } . "\n";
 
 	my $response = $event->raise({'temperature' => $scalar->{ 'temp' },
+				      'mode' => $scalar->{ 'tmode' } == 0 ? "off" :
+				                $scalar->{ 'tmode' } == 1 ? "heat" :
+				                $scalar->{ 'tmode' } == 2 ? "cool" :
+				                $scalar->{ 'tmode' } == 3 ? "auto" : "unknown",
+				      'target' => $scalar->{'t_heat'} || $scalar->{'t_cool'} || '99999',
+				      'fan' => $scalar->{'fmode'} == 0 ? "auto" :
+				               $scalar->{'fmode'} == 1 ? "circulate" :
+				               $scalar->{'fmode'} == 2 ? "on" : "unknown",
 				     }
 				    );
 
@@ -142,14 +154,45 @@ sub run_temperature_task(@) {
 	foreach my $d (@{$response->{'directives'}}) {
 	   if ($d->{'name'} eq 'radstat') {
 	     my $o = $d->{'options'};
-	     if ($o->{'message'}) {
+	     if ($o->{'action'} eq 'set_message') {
 	       radstat_request($ua, "$thermo_url/pma", "message set", {message => $o->{'message'}} );
-	     } elsif ($o->{'led'}) {
+	     } elsif ($o->{'action'} eq 'set_color') {
 	       my $color = ($o->{'led'} eq "green")  ? 1 :
                            ($o->{'led'} eq "yellow") ? 2 :
                            ($o->{'led'} eq "red")    ? 4 :
                                                        0 ;   # off
-	       radstat_request($ua, "$thermo_url/led", "LED color change ($color)" , {energy_led => $color});
+	       radstat_request($ua, "$thermo_url/led", "LED color change ($o->{led})" , {energy_led => $color});
+	     } elsif ($o->{'action'} eq 'set_temperature') {
+	       # my $m = $o->{'mode'} eq 'off'  ? 0 :
+               #         $o->{'mode'} eq 'heat' ? 1 :
+               #         $o->{'mode'} eq 'cool' ? 2 :
+               #         $o->{'mode'} eq 'auto' ? 3 :
+               #                                  0;
+       	       # radstat_request($ua, $thermo_url, "Set mode ($o->{'mode'}})" , 
+	       # 		       {tmode => $m}
+	       # 		      );
+       	       # radstat_request($ua, $thermo_url, "Set target temp ($o->{'temperature'})" , 
+	       # 		       {a_heat => int $o->{'temperature'}}
+	       # 		      );
+
+	       if ($o->{'mode'} eq 'heat') {
+		 radstat_request($ua, $thermo_url, "Set mode ($o->{'mode'})" , 
+				 {a_heat => int $o->{'temperature'}}
+				);
+	       } elsif ($o->{'mode'} eq 'cool') {
+		 radstat_request($ua, $thermo_url, "Set mode ($o->{'mode'})" , 
+				 {a_cool => int $o->{'temperature'}}
+				);
+	       } else {
+		 # radstat_request($ua, $thermo_url, "Set mode ($o->{'mode'}})" , 
+		 # 		 {tmode => $o->{'mode'}}
+		 # 		);
+		 
+	       }
+	     } elsif ($o->{'action'} eq 'turn_off') {
+	       radstat_request($ua, $thermo_url, "Set mode ($o->{'mode'}})" , 
+			       {tmode => 0}
+			      );
 	     }
 	   }
 	 }
@@ -188,12 +231,14 @@ sub read_config {
 sub radstat_request {
   my ($ua, $resource_uri, $message, $content ) = @_;
   my $post = HTTP::Request->new(POST => $resource_uri);
-  $post->content(to_json($content));
+  my $json_content = to_json($content);
+  warning "Sending $json_content";
+  $post->content($json_content);
   my $res  = $ua->request($post);
   if ($res->is_success()) {
     parse_response($res, $message);
   } else {
-    warning "HTTP request to $resource_uri failed: " . $res->message();
+    warning "HTTP request to $resource_uri failed: " .$res->message();
   }
   
 }
